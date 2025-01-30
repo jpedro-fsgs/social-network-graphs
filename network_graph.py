9ed599c62f243d2cda22df5083edad09c62654ce
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-01-30 15:52:28 -0300

    readme

--- network_graph.py
+++ network_graph.py
@@ -2,19 +2,26 @@ import networkx as nx
 
 def gerar_dados(G):
     number_of_nodes = G.number_of_nodes()
+    number_of_edges = G.number_of_edges()
 
     degrees = nx.degree(G)
     average_degree = sum(dict(degrees).values()) / number_of_nodes
 
-    average_clustering = nx.average_clustering(G)
-    average_shortest_path_length = nx.average_shortest_path_length(G)\
-          if number_of_nodes < 5000 else "O(n²), não vai rolar"
+    average_shortest_path_length = (
+        nx.average_shortest_path_length(G)
+        if number_of_nodes < 5000
+        else "O(n²), não vai rolar"
+    )
+
+    density = nx.density(G)
 
     return (
-        f"Conectividade Média: {average_degree}\n" +
-        f"Coeficiente de Agregação Médio: {average_clustering}\n" +
+        f"Número de nós: {number_of_nodes}\n"
+        f"Número de arestas: {number_of_edges}\n"
+        f"Conectividade Média: {average_degree}\n"
         f"Menor Caminho Médio: {average_shortest_path_length}\n"
-        )
+        f"Densidade da Rede: {density}\n"
+    )
 
 
 G_twitter = nx.read_edgelist("twitter_combined.txt", create_using=nx.Graph, nodetype=int)
@@ -23,6 +30,5 @@ G_facebook = nx.read_edgelist("facebook_combined.txt", create_using=nx.Graph, no
 nx.write_graphml(G_twitter, "grafo_twitter.graphml")
 nx.write_graphml(G_facebook, "grafo_facebook.graphml")
 
-print("Twitter:\n\n" + gerar_dados(G_twitter))
 print("Facebook:\n\n" + gerar_dados(G_facebook))
-
+print("Twitter:\n\n" + gerar_dados(G_twitter))
