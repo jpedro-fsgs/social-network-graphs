76b6674f17a10f3957496fc629515bb75d56bfbd
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-01-30 10:48:00 -0300

    sources added

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import networkx as nx
-from faker import Faker
 
-fake = Faker("pt_BR")
+G = nx.read_edgelist("twitter_combined.txt", create_using=nx.Graph, nodetype=int)
 G = nx.read_edgelist("facebook_combined.txt", create_using=nx.Graph, nodetype=int)
 
-
-for node in G.nodes:
-    G.add_node(node, name=fake.name())
-
-
-print(G.nodes.data())
-nx.write_graphml(G, "grafo.graphml")
+nx.write_graphml(G, "grafo_twitter.graphml")
+nx.write_graphml(G, "grafo_facebook.graphml")
